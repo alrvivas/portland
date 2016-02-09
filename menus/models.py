3aed351f6e47fbbc9b570543ae6d9d01e6638ddf
alrvivas <alr.vivas@gmail.com>
2016-02-09 09:36:24 -0600

    v0.1

--- menus/models.py
+++ menus/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+from imagekit.models import ImageSpecField
+from imagekit.processors import ResizeToFill
 
-# Create your models here.
+
+
+class Cliente(models.Model):
+	user = models.OneToOneField(User,null=True, blank=True)
+	fecha_ingreso = models.DateField(null=True)
+	direccion = models.CharField(max_length=140,null=True)
+	
--- portland/settings.py
+++ portland/settings.py
@@ -80,12 +80,35 @@ WSGI_APPLICATION = 'portland.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
+# Database
+# https://docs.djangoproject.com/en/1.8/ref/settings/#databases
+# Database
+# https://docs.djangoproject.com/en/1.8/ref/settings/#databases
+
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db2.sqlite3'),
     }
 }
+"""
+"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'campoapp',
+        'USER': 'postgres',
+        'PASSWORD': '123456',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
+
+"""
+import dj_database_url
+DATABASES = {'default': dj_database_url.config()}
+DATABASES['default'] =  dj_database_url.config()
 
 
 # Internationalization
