f005731b80841d1b8989867aceabf4f3561a9fd4
alrvivas <alr.vivas@gmail.com>
2016-02-10 17:31:22 -0600

    v0.1

--- pedidos/urls.py
+++ pedidos/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
-	url(r'^template/(?P<template_id>[-\w]+)$', 'empleados.views.template', name='template'), 
+	url(r'^template/(?P<template_id>[-\w]+)$', 'pedidos.views.template', name='template'), 
 	
 )
